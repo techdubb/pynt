ae62f06de5382619eff2e9ad8ab8fc8fba3888fc
Matthew Hokanson <m@h0ke.com>
2011-05-28 00:49:17 -0700

    changed .md to .markdown. removed some errant comments.

--- pynt/beer.py
+++ pynt/beer.py
@@ -1,15 +1,4 @@
-"""
-	Pynt is a Python client that wraps the Open Beer Database API.
-
-	Questions, comments? m@h0ke.com
-"""
-
-__author__ = "Matthew Hokanson <m@h0ke.com>"
-__version__ = "0.1.0"
-
-
 from request import Request
-
 
 class Beer(object):
 
